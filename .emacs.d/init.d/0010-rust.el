16adf81510847592dc03362b4998d6fe87b8b570
Jordan Brown <jordan.keith.brown@gmail.com>
2016-06-26 22:32:43 -0500

    Add Rust hooks

--- .emacs.d/init.d/0010-rust.el
+++ .emacs.d/init.d/0010-rust.el
@@ -1,3 +1,24 @@
 (require 'flymake-rust)
 (add-hook 'rust-mode-hook
           'flymake-rust-load)
+(add-hook 'rust-mode-hook
+          'cargo-minor-mode)
+
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c <tab>")
+                           #'rust-format-buffer)))
+
+(setq racer-cmd
+      "~/.cargo/bin/racer")
+(setq racer-rust-src-path
+      "~/.code/rust-lang/rust")
+
+(add-hook 'rust-mode-hook
+          #'racer-mode)
+(add-hook 'racer-mode-hook
+          #'eldoc-mode)
+(add-hook 'racer-mode-hook
+          #'company-mode)
+(add-hook 'flycheck-mode-hook
+          #'flycheck-rust-setup)
