7fc55ee12ec922050f67c95497bc1665d074422a
Jordan Brown <mrhmouse@gmail.com>
2016-09-02 10:19:56 -0400

    Set colors for erc

--- .emacs.d/init.d/0010-customize.el
+++ .emacs.d/init.d/0010-customize.el
@@ -32,6 +32,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-input-face ((t (:foreground "cyan"))))
+ '(erc-my-nick-face ((t (:foreground "cyan" :weight bold))))
  '(magit-diff-added ((t (:background "#ddffdd" :foreground "#000000"))))
  '(magit-diff-added-highlight ((t (:background "#cceecc" :foreground "#000000"))))
  '(magit-diff-base ((t (:background "#ffffcc" :foreground "#000000"))))
